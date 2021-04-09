12494c774648d42a1591ceea59bc905f7dfc11f1
Yoli Nikolova <yoli9818@gmail.com>
2021-04-09 06:09:30 +0000

    First milestone

---
+++ src/main/java/com/prime/rushhour/entities/Activity.java
@@ -0,0 +1,32 @@
+package com.prime.rushhour.entities;
+
+public class Activity {
+    private String name;
+    private double duration;
+    private double price;
+    //private List<Appointment> appointments;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
