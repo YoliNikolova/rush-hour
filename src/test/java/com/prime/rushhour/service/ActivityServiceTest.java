ef7bbe28d7b7bb0d64ee75ceaf52cce0efe7c9ba
Yoli Nikolova <yoli9818@gmail.com>
2021-05-04 18:15:03 +0300

    unit testing-services

--- src/test/java/com/prime/rushhour/service/ActivityServiceTest.java
+++ src/test/java/com/prime/rushhour/service/ActivityServiceTest.java
@@ -1,12 +1,10 @@
 package com.prime.rushhour.service;
 
 import com.prime.rushhour.entities.Activity;
+import com.prime.rushhour.exception.ActivityNotFoundException;
 import com.prime.rushhour.models.ActivityDTO;
-import com.prime.rushhour.models.UserResponseDTO;
 import com.prime.rushhour.repository.ActivityRepository;
-import com.prime.rushhour.repository.UserRepository;
 import com.prime.rushhour.services.ActivityService;
-import com.prime.rushhour.services.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -14,7 +12,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -32,22 +33,22 @@ public class ActivityServiceTest {
     private ModelMapper modelMapper;
 
     @Test
-    public void addNeeActivitySuccess(){
-        Activity activity = new Activity("Fitness",60,5.00);
-        ActivityDTO activityDTO = new ActivityDTO("Fitness",60,5.00);
+    public void addNewActivitySuccess() {
+        Activity activity = new Activity("Fitness", 60, 5.00);
+        ActivityDTO activityDTO = new ActivityDTO("Fitness", 60, 5.00);
 
         when(activityRepository.save(any(Activity.class))).thenReturn(activity);
-        Mockito.when(modelMapper.map(any(), any())).thenReturn(activity);
-        Mockito.when(modelMapper.map(any(), any())).thenReturn(activityDTO);
-        ActivityDTO addActivity = activityService.add(new ActivityDTO());
+        Mockito.when(modelMapper.map(activityDTO, Activity.class)).thenReturn(activity);
+        Mockito.when(modelMapper.map(activity, ActivityDTO.class)).thenReturn(activityDTO);
+        ActivityDTO addActivity = activityService.add(activityDTO);
 
-       assertEquals(addActivity.getName(),activity.getName());
+        assertEquals(addActivity.getName(), activity.getName());
     }
 
     @Test
-    public void getActivityByIdSuccess(){
-        Activity activity = new Activity("Fitness",60,5.00);
-        ActivityDTO activityDTO = new ActivityDTO("Fitness",60,5.00);
+    public void getActivityByIdSuccess() {
+        Activity activity = new Activity("Fitness", 60, 5.00);
+        ActivityDTO activityDTO = new ActivityDTO("Fitness", 60, 5.00);
 
         when(activityRepository.findById(anyInt())).thenReturn(Optional.of(activity));
         Mockito.when(modelMapper.map(any(), any())).thenReturn(activityDTO);
@@ -56,4 +57,61 @@ public class ActivityServiceTest {
         assertEquals(activity.getName(), activityById.getName());
     }
 
+    @Test(expected = ActivityNotFoundException.class)
+    public void getActivityByIdThrowException() {
+        when(activityRepository.findById(anyInt())).thenThrow(ActivityNotFoundException.class);
+        activityService.getById(anyInt());
+    }
+
+    @Test
+    public void getAllActivitiesSuccess() {
+        Pageable pageable = Mockito.mock(Pageable.class);
+        Page<Activity> activities = Mockito.mock(Page.class);
+        when(activityRepository.findAll(any(Pageable.class))).thenReturn(activities);
+        List<ActivityDTO> listOfActivities = activityService.getAll(pageable);
+        assertEquals(listOfActivities.size(), activities.getSize());
+    }
+
+    @Test
+    public void updateActivityByIdSuccess() {
+        Activity activity = new Activity("Fitness", 60, 5.00);
+        ActivityDTO activityDTO = new ActivityDTO("Fitness", 60, 5.00);
+
+        Mockito.when(modelMapper.map(activityDTO, Activity.class)).thenReturn(activity);
+        activity.setId(1);
+        when(activityRepository.save(any(Activity.class))).thenReturn(activity);
+        when(activityRepository.existsById(1)).thenReturn(true);
+
+        Mockito.when(modelMapper.map(activity, ActivityDTO.class)).thenReturn(activityDTO);
+        ActivityDTO updateById = activityService.updateById(activityDTO, 1);
+
+        assertEquals(updateById.getName(), activity.getName());
+    }
+
+    @Test(expected = ActivityNotFoundException.class)
+    public void updateActivityByIdThrowException() {
+        Activity activity = new Activity("Fitness", 60, 5.00);
+        ActivityDTO activityDTO = new ActivityDTO("Fitness", 60, 5.00);
+
+        Mockito.when(modelMapper.map(activityDTO, Activity.class)).thenReturn(activity);
+        activity.setId(1);
+        when(activityRepository.existsById(1)).thenReturn(false);
+        activityService.updateById(activityDTO, 1);
+        verify(activityRepository, times(0)).save(activity);
+    }
+
+    @Test
+    public void deleteActivityByIdSuccess() {
+        when(activityRepository.existsById(1)).thenReturn(true);
+        doNothing().when(activityRepository).deleteById(1);
+        activityService.delete(1);
+        verify(activityRepository, times(1)).deleteById(1);
+    }
+
+    @Test(expected = ActivityNotFoundException.class)
+    public void deleteActivityByIdThrowException() {
+        when(activityRepository.existsById(1)).thenReturn(false);
+        activityService.delete(1);
+        verify(activityRepository, times(0)).deleteById(1);
+    }
 }
--- src/test/java/com/prime/rushhour/service/UserServiceTest.java
+++ src/test/java/com/prime/rushhour/service/UserServiceTest.java
@@ -2,15 +2,16 @@ package com.prime.rushhour.service;
 
 import com.prime.rushhour.entities.Role;
 import com.prime.rushhour.entities.User;
+import com.prime.rushhour.exception.ThisEmailAlreadyExistsException;
 import com.prime.rushhour.exception.UserNotFoundException;
+import com.prime.rushhour.models.RegisterRequest;
 import com.prime.rushhour.models.UserRequestDTO;
 import com.prime.rushhour.models.UserResponseDTO;
+import com.prime.rushhour.repository.RoleRepository;
 import com.prime.rushhour.repository.UserRepository;
 import com.prime.rushhour.services.UserService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -19,13 +20,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.*;
 
 import static org.mockito.ArgumentMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
@@ -33,6 +30,9 @@ import static org.mockito.Mockito.*;
 public class UserServiceTest {
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private RoleRepository roleRepository;
 
     @InjectMocks
     private UserService userService;
@@ -57,46 +57,154 @@ public class UserServiceTest {
     @Test(expected = UserNotFoundException.class)
     public void getUserByIdThrowException() {
         when(userRepository.findById(anyInt())).thenThrow(UserNotFoundException.class);
-        UserResponseDTO userById = userService.getById(anyInt());
+        userService.getById(anyInt());
     }
 
     @Test
     public void getAllUsersSuccess() {
-        User user = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+        UserResponseDTO responseUser = new UserResponseDTO("Yoli", "Nikolova", "yoli@abv.bg");
+        Pageable pageable = Mockito.mock(Pageable.class);
         Page<User> users = Mockito.mock(Page.class);
-
-        UserResponseDTO responseUser = new UserResponseDTO("Yoli", "Nikolova", "yoli@abv.bg");
-
-       /* when(userRepository.findAll(any(Pageable.class))).thenReturn(users);
-        when(users.stream()).thenAnswer(i->Stream.of(users));
-        Mockito.when(modelMapper.map(any(), any())).thenReturn(responseUser);
-        InOrder inOrder = Mockito.inOrder(users);
-        List<UserResponseDTO> userById = userService.getAll(any(Pageable.class));
-        assertEquals(userById.size(), users.getSize());
-
-        */
+        when(userRepository.findAll(any(Pageable.class))).thenReturn(users);
+        List<UserResponseDTO> listOfUsers = userService.getAll(pageable);
+        assertEquals(listOfUsers.size(), users.getSize());
     }
 
     @Test
-    public void addNewUserSuccess() {
+    public void addNewUserSuccessWithExistingRoles() {
         User user = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
-        User userWithSetRoles = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
         UserResponseDTO responseUser = new UserResponseDTO("Yoli", "Nikolova", "yoli@abv.bg");
-        UserRequestDTO userRequestDTO = new UserRequestDTO();
-        UserRequestDTO userForRequest = new UserRequestDTO("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+        UserRequestDTO requestUser = new UserRequestDTO("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
 
+        Mockito.when(modelMapper.map(requestUser, User.class)).thenReturn(user);
+        when(roleRepository.findByName(anyString())).thenReturn(Optional.of(new Role()));
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        Mockito.when(modelMapper.map(user, UserResponseDTO.class)).thenReturn(responseUser);
+        UserResponseDTO addUser = userService.add(requestUser);
 
-        when(userRepository.save(any(User.class))).thenReturn(userWithSetRoles);
-        Mockito.when(modelMapper.map(org.mockito.ArgumentMatchers.any(),any())).thenReturn(user);
-        Mockito.when(modelMapper.map(any(),any())).thenReturn(responseUser);
-        UserResponseDTO addUser = userService.add(userRequestDTO);
-
-        assertEquals(responseUser.getFirstName(), user.getFirstName());
-
+        assertSame(addUser.getEmail(), requestUser.getEmail());
     }
 
     @Test
-    public void registerUserSuccess(){
+    public void addNewUserSuccessWithNotExistingRoles() {
+        User user = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+        UserResponseDTO responseUser = new UserResponseDTO("Yoli", "Nikolova", "yoli@abv.bg");
+        UserRequestDTO requestUser = new UserRequestDTO("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
 
+        Mockito.when(modelMapper.map(requestUser, User.class)).thenReturn(user);
+        when(roleRepository.findByName(anyString())).thenReturn(Optional.empty());
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        Mockito.when(modelMapper.map(user, UserResponseDTO.class)).thenReturn(responseUser);
+        UserResponseDTO addUser = userService.add(requestUser);
+
+        assertSame(addUser.getEmail(), requestUser.getEmail());
+    }
+
+    @Test
+    public void updateUserWithExistingRoles(){
+        User user = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+        UserResponseDTO responseUser = new UserResponseDTO("Yoli", "Nikolova", "yoli@abv.bg");
+        UserRequestDTO requestUser = new UserRequestDTO("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+
+        Mockito.when(modelMapper.map(requestUser, User.class)).thenReturn(user);
+        when(userRepository.existsById(1)).thenReturn(true);
+        when(roleRepository.findByName(anyString())).thenReturn(Optional.of(new Role()));
+        user.setId(1);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        Mockito.when(modelMapper.map(user, UserResponseDTO.class)).thenReturn(responseUser);
+        UserResponseDTO response = userService.updateById(requestUser, 1);
+
+        assertSame(response.getEmail(),requestUser.getEmail());
+    }
+
+    @Test
+    public void updateUserWithNotExistingRoles(){
+        User user = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+        UserResponseDTO responseUser = new UserResponseDTO("Yoli", "Nikolova", "yoli@abv.bg");
+        UserRequestDTO requestUser = new UserRequestDTO("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+
+        Mockito.when(modelMapper.map(requestUser, User.class)).thenReturn(user);
+        when(userRepository.existsById(1)).thenReturn(true);
+        when(roleRepository.findByName(anyString())).thenReturn(Optional.empty());
+        user.setId(1);
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        Mockito.when(modelMapper.map(user, UserResponseDTO.class)).thenReturn(responseUser);
+        UserResponseDTO response = userService.updateById(requestUser, 1);
+
+        assertSame(response.getEmail(),requestUser.getEmail());
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    public void updateUserThrowException(){
+        User user = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+        UserRequestDTO requestUser = new UserRequestDTO("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+
+        Mockito.when(modelMapper.map(requestUser, User.class)).thenReturn(user);
+      //  when(userRepository.existsById(1)).thenReturn(false);
+        userService.updateById(requestUser, 1);
+
+        verify(userRepository,times(0)).save(user);
+    }
+
+    @Test
+    public void registerUserWithExistingRoles() {
+        User user = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+        UserResponseDTO responseUser = new UserResponseDTO("Yoli", "Nikolova", "yoli@abv.bg");
+        RegisterRequest request = new RegisterRequest("Yoli","Nikolova","yoli@abv.bg","yoli9898");
+        Role role = new Role("ROLE_USER");
+
+        Mockito.when(modelMapper.map(request, User.class)).thenReturn(user);
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        when(roleRepository.findByName(anyString())).thenReturn(Optional.of(role));
+        user.setRoles(Collections.singletonList(role));
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        Mockito.when(modelMapper.map(user, UserResponseDTO.class)).thenReturn(responseUser);
+        UserResponseDTO response = userService.registerUser(request);
+
+        assertSame(response.getEmail(),request.getEmail());
+    }
+
+    @Test
+    public void registerUserWithNotExistingRoles() {
+        User user = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+        UserResponseDTO responseUser = new UserResponseDTO("Yoli", "Nikolova", "yoli@abv.bg");
+        RegisterRequest request = new RegisterRequest("Yoli","Nikolova","yoli@abv.bg","yoli9898");
+
+        Mockito.when(modelMapper.map(request, User.class)).thenReturn(user);
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        when(roleRepository.findByName(anyString())).thenReturn(Optional.empty());
+        user.setRoles(Collections.singletonList(new Role("USER_ROLE")));
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        Mockito.when(modelMapper.map(user, UserResponseDTO.class)).thenReturn(responseUser);
+        UserResponseDTO response = userService.registerUser(request);
+
+        assertSame(response.getEmail(),request.getEmail());
+    }
+
+    @Test(expected = ThisEmailAlreadyExistsException.class)
+    public void registerUserThrowException() {
+        User user = new User("Yoli", "Nikolova", "yoli@abv.bg", "yoli9818", Arrays.asList(new Role("User")));
+        RegisterRequest request = new RegisterRequest("Yoli","Nikolova","yoli@abv.bg","yoli9898");
+
+        Mockito.when(modelMapper.map(request, User.class)).thenReturn(user);
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+        userService.registerUser(request);
+
+        verify(userRepository,times(0)).save(user);
+    }
+
+    @Test
+    public void deleteUserByIdSuccess() {
+        when(userRepository.existsById(1)).thenReturn(true);
+        doNothing().when(userRepository).deleteById(1);
+        userService.delete(1);
+        verify(userRepository, times(1)).deleteById(1);
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    public void deleteUserByIdThrowException() {
+        when(userRepository.existsById(1)).thenReturn(false);
+        userService.delete(1);
+        verify(userRepository, times(0)).deleteById(1);
     }
 }
